fe05c84226d8d8a3863fe767271a599df888ed71
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2026-03-26 16:23:30 +0530

    Time: 6 ms (24.48%), Space: 161.7 MB (41.26%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -1,30 +1,25 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        long total = 0;
-        
-        for (int[] row : grid)
-            for (int x : row)
-                total += x;
-        
-        if ((total & 1) == 1) return false;
-        
-        long target = total / 2, sum = 0;
-        
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n; j++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+        double sum =0;
+        for(int[] row: grid){
+            for(int x: row){
+                sum+=x;
+            }
         }
-        
-        sum = 0;
-        
-        for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < m; i++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+        double rowSum =0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                rowSum += grid[i][j];
+            }
+            if(rowSum == sum/2) return true;
         }
-        
+        double columnSum =0;
+        for(int j=0;j<grid[0].length;j++){
+            for(int i=0;i<grid.length;i++){
+                columnSum+=grid[i][j];
+            }
+            if(columnSum == sum/2) return true;
+        }
         return false;
     }
 }
