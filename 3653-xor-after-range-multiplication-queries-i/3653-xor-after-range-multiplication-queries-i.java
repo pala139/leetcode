7104ec88ed70175eabae3cd90430080801e95805
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2026-04-08 21:59:28 +0530

    Time: 60 ms (57.33%), Space: 47.8 MB (69.33%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -1,18 +1,23 @@
 class Solution {
     public int xorAfterQueries(int[] nums, int[][] queries) {
-        long MOD = 1_000_000_007;
+        long MOD = (long)10e8+7;
+        int n = nums.length;
+        long[] arr = new long[n];
+        for(int i=0;i<n;i++){
+            arr[i] = (long)nums[i];
+        }
         for(int[] q: queries){
             int l = q[0];
             int r = q[1];
             int k = q[2];
             int v = q[3];
             for(int i=l;i<=r;i+=k){
-                nums[i] = (int)(((long)nums[i]*v)%MOD);
+                arr[i] = (arr[i]*(long)v)%MOD;
             }
         }
-        int res = 0;
-        for(int ele: nums)
+        long res = 0;
+        for(long ele: arr)
             res^=ele;
-        return res;
+        return (int)res;
     }
 }
