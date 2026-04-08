bbacf5399864a3db77c62519bb23aecd2f904664
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2026-04-08 22:01:07 +0530

    Time: 55 ms (97.33%), Space: 47.7 MB (90.67%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -1,19 +1,20 @@
 class Solution {
-    public int xorAfterQueries(int[] nums, int[][] queries) {
-        long MOD = (long)10e8+7;
-        int n = nums.length;
-        long[] arr = new long[n];
+    long MOD =(long)10e8+7;
+    public int xorAfterQueries(int[] nums, int[][] q) {
+        int n= nums.length;
+        long arr[] =new long[n];
         for(int i=0;i<n;i++){
             arr[i] = (long)nums[i];
         }
-        for(int[] q: queries){
-            for(int i=q[0];i<=q[1];i+=q[2]){
-                arr[i] = (arr[i]*(long)q[3])%MOD;
+        for(int i=0;i<q.length;i++){
+            for(int j=q[i][0];j<=q[i][1];j+=q[i][2]){
+                arr[j]=(arr[j]*(long)q[i][3])%MOD;
             }
         }
-        long res = 0;
-        for(int i=0;i<n;i++)
-            res ^=arr[i];
-        return (int)res;
+        long ans =0;
+        for(int i=0;i<n;i++){
+            ans = ans^arr[i];
+        }
+        return (int)ans;
     }
 }
