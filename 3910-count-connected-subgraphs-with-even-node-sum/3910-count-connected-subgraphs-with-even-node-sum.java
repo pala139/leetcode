1a90b719a8a18f690da2d37ea16dd866754ada77
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2026-04-27 00:04:50 +0530

    Time: 53 ms (100%), Space: 47.3 MB (100%) - LeetHub

--- 3910-count-connected-subgraphs-with-even-node-sum/3910-count-connected-subgraphs-with-even-node-sum.java
+++ 3910-count-connected-subgraphs-with-even-node-sum/3910-count-connected-subgraphs-with-even-node-sum.java
@@ -1,64 +1,51 @@
-import java.util.*;
-
 class Solution {
-
-    void dfs(int u, List<List<Integer>> adj, int[] vis) {
-        if (vis[u] == 1) return;
-        vis[u] = 1;
-
-        for (int v : adj.get(u)) {
+    private void dfs(int node, int[] vis, ArrayList<ArrayList<Integer>> adj) {
+        if (vis[node] == 1)
+            return;
+        vis[node] = 1;
+        for (int v : adj.get(node)) {
             if (vis[v] == 0) {
-                dfs(v, adj, vis);
+                dfs(v, vis, adj);
             }
+
         }
     }
 
     public int evenSumSubgraphs(int[] nums, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = nums.length;
-
-        // Build adjacency list
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
-        for (int[] e : edges) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
         }
 
-        int[] vis = new int[n];
-        int ans = 0;
-
-        // Iterate over all subsets
-        for (int mask = 1; mask < (1 << n); mask++) {
-
-            Arrays.fill(vis, -1);
-            List<Integer> subset = new ArrayList<>();
+        for (int mask = 1; mask < 1 << n; mask++) {
+            List<Integer> subset = new LinkedList<Integer>();
             int sum = 0;
-
-            // Build subset
-            for (int j = 0; j < n; j++) {
-                if ((mask & (1 << j)) != 0) {
-                    subset.add(j);
-                    vis[j] = 0; // mark as present but not visited
-                    sum += nums[j];
+            int[] vis = new int[n];
+            Arrays.fill(vis, -1);
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    subset.add(i);
+                    sum += nums[i];
+                    vis[i] = 0;
                 }
             }
+            dfs(subset.get(0), vis, adj);
 
-            // Run DFS from first node of subset
-            dfs(subset.get(subset.size()-1), adj, vis);
-
-            // Count visited nodes
-            int count = 0;
-            for (int v : vis) {
-                if (v == 1) count++;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (vis[i] == 1)
+                    cnt++;
             }
-
-            // Check connectivity + even sum
-            if (count == subset.size() && sum % 2 == 0) {
-                ans++;
+            if (cnt == subset.size() && sum % 2 == 0) {
+                res++;
             }
         }
-
-        return ans;
+        return res;
     }
 }
